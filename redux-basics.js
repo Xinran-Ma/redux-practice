049f07d7f06b24d9c9b85ff375e3348f8f59920e
Xinran-Ma <xinran_ma@hotmail.com>
2020-05-27 23:27:14 +1000

    Added subscription to redux

--- redux-basics.js
+++ redux-basics.js
@@ -1,10 +1,11 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-
+// Initial State
 const initialState = {
     counter: 0
 }
+
 // Reducer
 const rootReducer = (state = initialState, action) => {
     if (action.type === 'INC_COUNTER') {
@@ -26,10 +27,11 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => console.log('[Subscription]', store.getState()))
 
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState())
 
-// Subscription
